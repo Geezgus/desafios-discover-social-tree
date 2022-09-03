dbfb7fbf9ce52bd9f49073672e08d46b8202fe75
Gustavo Duarte <gutorap.gh@gmail.com>
2022-09-02 23:49:01 -0300

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# ROCKETSEAT CHALLENGES - Social Tree
+
+Descrição do desafio: [Desafio: Social Tree](https://efficient-sloth-d85.notion.site/Desafio-Social-Tree-a4008e467a3248c4b05c97cf78aea44f)
+
+A idealização do app é de completa autoria da [Rocketseat](https://www.rocketseat.com.br/)
+
+![Protótipo do website](https://efficient-sloth-d85.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fb0302905-3a2d-4897-9a7b-cb8c94ce7e38%2FiPhone_13.png?table=block&id=bd1c201f-fba0-41e9-b162-554e5f6d3e8c&spaceId=08f749ff-d06d-49a8-a488-9846e081b224&width=512&userId=&cache=v2 "Protótipo do website")
+
+Feito com 💜 por Rocketseat!
---
+++ index.js
@@ -0,0 +1,71 @@
+const onAssetsSuccess = new CustomEvent("assetsSuccess", {});
+
+const main = () => {
+  const assets = {
+    "icon-twitter": "/assets/twitter.svg",
+    "icon-twitch": "/assets/twitch.svg",
+    "icon-instagram": "/assets/instagram.svg",
+    "icon-linkedin": "/assets/linkedin.svg",
+    "icon-github": "/assets/github.svg",
+  };
+
+  addEventListener("assetsSuccess", () => {
+    for (const ID in assets) {
+      const asset = assets[ID];
+
+      toIcon(ID, `${asset}`);
+
+      const separators = document.getElementsByClassName("separator");
+      for (const separator of separators) {
+        separator.classList.add("disabled");
+      }
+    }
+  });
+
+  requestAllInTarget(assets, () => {
+    dispatchEvent(onAssetsSuccess);
+  });
+};
+
+const requestAllInTarget = (target, onSuccessOfAll) => {
+  let requestSucessCount = 0;
+
+  for (const key in target) {
+    const asset = target[key];
+
+    const requestSucess = () => {
+      if (++requestSucessCount === Object.keys(target).length) {
+        onSuccessOfAll();
+      }
+    };
+
+    const requestFailure = () => {};
+
+    requestAsset(asset, requestSucess, requestFailure);
+  }
+};
+
+const requestAsset = (asset, onSuccess, onFailure) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("load", () => {
+    if (request.status === 200) {
+      onSuccess();
+    } else {
+      onFailure();
+    }
+  });
+
+  request.open("GET", asset, true);
+  request.send(null);
+};
+
+const toIcon = (ID, iconPath) => {
+  document.getElementById(ID).style.content = `url("${iconPath}")`;
+};
+
+const disable = (element) => {
+  element.classList.add("disabled");
+};
+
+main();
