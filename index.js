7ff26cfb157bae19ffc6e0ba985f2ee5f2384bbd
Gustavo Duarte <gutorap.gh@gmail.com>
2022-09-04 04:26:52 -0300

    BREAKING CHANGE:  new icon source (previously, svg files. now, custom typeface) BREAKING CHANGE:  complete rewrite of other-links' "iconfy" feature. refactor:         rename of `.disabled` class to `.hidden` feat:             new focus highlight on user-tag, custom-links and other-links

--- index.js
+++ index.js
@@ -1,69 +1,33 @@
-const onAssetsSuccess = new CustomEvent("assetsSuccess", {});
-
 const main = () => {
-  const assets = {
-    "icon-twitter": "./assets/twitter.svg",
-    "icon-twitch": "./assets/twitch.svg",
-    "icon-instagram": "./assets/instagram.svg",
-    "icon-linkedin": "./assets/linkedin.svg",
-    "icon-github": "./assets/github.svg",
-  };
-
-  addEventListener("assetsSuccess", () => {
-    for (const ID in assets) {
-      const asset = assets[ID];
-
-      toIcon(ID, `${asset}`);
-
-      const separators = document.getElementsByClassName("separator");
-      for (const separator of separators) {
-        separator.classList.add("disabled");
-      }
+  document.fonts.onloadingdone = () => {
+    if (isFontPresent("Social-Icons")) {
+      formatIcons();
+      hideSeparators();
     }
-  });
-
-  requestAllInTarget(assets, () => {
-    dispatchEvent(onAssetsSuccess);
-  });
+  };
 };
 
-const requestAllInTarget = (target, onSuccessOfAll) => {
-  let requestSucessCount = 0;
-
-  for (const key in target) {
-    const asset = target[key];
-
-    const requestSucess = () => {
-      if (++requestSucessCount === Object.keys(target).length) {
-        onSuccessOfAll();
-      }
-    };
-
-    requestAsset(asset, requestSucess);
+const formatIcons = () => {
+  const icons = document.getElementsByClassName("icon");
+  for (const icon of icons) {
+    icon.textContent = "";
+    icon.style.fontSize = "2.4rem";
   }
 };
 
-const requestAsset = (asset, onSuccess, onFailure) => {
-  const request = new XMLHttpRequest();
-
-  request.addEventListener("load", () => {
-    if (request.status === 200) {
-      onSuccess();
-    } else {
-      onFailure();
-    }
-  });
-
-  request.open("GET", asset, true);
-  request.send(null);
+const hideSeparators = () => {
+  const separators = document.getElementsByClassName("separator");
+  for (const separator of separators) {
+    hide(separator);
+  }
 };
 
-const toIcon = (ID, iconPath) => {
-  document.getElementById(ID).style.content = `url("${iconPath}")`;
+const isFontPresent = (fontFamily) => {
+  return document.fonts.check(`0 ${fontFamily}`);
 };
 
-const disable = (element) => {
-  element.classList.add("disabled");
+const hide = (element) => {
+  element.classList.add("hidden");
 };
 
 main();
